2f2d5f00cd1385ad5b498d8abe5f660d69ff8b67
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2023-05-25 00:53:15 +0200

    part2

--- app/src/main/java/com/mkrdeveloper/weatherappexample/MainActivity.kt
+++ app/src/main/java/com/mkrdeveloper/weatherappexample/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.Toast
 import com.mkrdeveloper.weatherappexample.databinding.ActivityMainBinding
 import com.mkrdeveloper.weatherappexample.utils.RetrofitInstance
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         getCurrentWeather()
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     private fun getCurrentWeather() {
         GlobalScope.launch(Dispatchers.IO) {
            val response = try {
@@ -39,7 +44,45 @@ class MainActivity : AppCompatActivity() {
 
             if (response.isSuccessful && response.body()!= null){
                 withContext(Dispatchers.Main){
-                    binding.tvTemp.text = "tem: ${response.body()!!.main.temp}"
+
+                    val data = response.body()!!
+
+                    val iconId = data.weather[0].icon
+
+                    val imgUrl = "https://openweathermap.org/img/w/$iconId.png"
+
+                    Picasso.get().load(imgUrl).into(binding.imgWeather)
+
+                    binding.tvSunset.text =
+                        SimpleDateFormat(
+                            "hh:mm a",
+                            Locale.ENGLISH
+                        ).format(data.sys.sunset * 1000)
+
+                    binding.tvSunrise.text =
+                        SimpleDateFormat(
+                            "hh:mm a",
+                            Locale.ENGLISH
+                        ).format(data.sys.sunrise * 1000)
+
+                    binding.apply {
+                        tvStatus.text = data.weather[0].description
+                        tvWind.text = "${ data.wind.speed.toString() } KM/H"
+                        tvLocation.text = "${data.name}\n${data.sys.country}"
+                        tvTemp.text = "${data.main.temp.toInt()}°C"
+                        tvFeelsLike.text = "Feels like: ${data.main.feels_like.toInt()}°C"
+                        tvMinTemp.text = "Min temp: ${data.main.temp_min.toInt()}°C"
+                        tvMaxTemp.text = "Max temp: ${data.main.temp_max.toInt()}°C"
+                        tvHumidity.text = "${data.main.humidity}%"
+                        tvPressure.text = "${data.main.pressure}hPa"
+                        tvUpdateTime.text = "Last Update: ${
+                            SimpleDateFormat(
+                                "hh:mm a",
+                                Locale.ENGLISH
+                            ).format(data.dt * 1000)
+                        }"
+                    }
+
                 }
             }
         }
